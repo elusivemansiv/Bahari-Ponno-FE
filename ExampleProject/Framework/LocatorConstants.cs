067b187d245ee585452af35a9380f5f25be327e3
Khan,Mansib  Ahsan <97165527+elusivemansiv@users.noreply.github.com>
2026-02-09 18:07:38 +0600

    Delete ExampleProject directory

--- ExampleProject/Framework/LocatorConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExampleProject.Framework
-{
-    public class LocatorConstants
-    {
-        public const string PreciseTextLocator = "//*[text()='{0}']";
-        public const string PartialTextLocator = "//*[contains(text(),'{0}')]";
-    }
-}
